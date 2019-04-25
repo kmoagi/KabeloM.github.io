393f551cc7545da24b554faf93b609ea19ddb937
kabelo <kabelomg@gmail.com>
2019-04-25 15:05:30 +0200

    Tribute page (Phillip Thabane)

---
+++ java/tribute.java
@@ -0,0 +1,22 @@
+
+const projectName = 'tribute-page';
+localStorage.setItem('example_project', 'Tribute Page');
+
+const projectName = 'tribute-page';
+localStorage.setItem('example_project', 'Tribute Page');
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
